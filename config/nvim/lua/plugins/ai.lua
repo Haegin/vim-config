4d43b5a8df2cf6c79578b0d5d80fe6b8eb257972
Harry Porter-Mills <harry.porter-mills@instacart.com>
2025-02-14 14:35:57 -0500

    Add Copilot config

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -1,5 +1,36 @@
 return {
-	{ "zbirenbaum/copilot.lua", config = true },
+	{
+		"zbirenbaum/copilot.lua",
+		opts = {
+			panel = {
+				enabled = true,
+				auto_refresh = false,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = "<M-CR>",
+				},
+				layout = {
+					position = "bottom", -- | top | left | right | horizontal | vertical
+					ratio = 0.4,
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = true,
+				debounce = 75,
+				keymap = {
+					accept = "<tab>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+		},
+	},
 	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
